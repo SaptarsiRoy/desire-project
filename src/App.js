69e7e090b5c208f0d3cce9a6aa8130f8606d50d0
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-08-31 00:24:37 +0530

    completed meme cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import useMeme from "./hooks/useMeme";
 
 function App() {
-  return <div className="App"></div>;
+  const { meme } = useMeme();
+
+  console.log(meme);
+
+  return (
+    <div className="app">
+      {meme &&
+        meme.map((m) =>
+          m.isVideo ? (
+            <video key={m.id} src={m.url} autoPlay loop muted />
+          ) : (
+            <img key={m.id} src={m.url} alt="meme" />
+          )
+        )}
+    </div>
+  );
 }
 
 export default App;
