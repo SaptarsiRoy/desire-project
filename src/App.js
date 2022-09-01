a7114597302fcfa3e9c4dce765a66c2414586864
Saptarsi Roy <saptarsiroy12@gmail.com>
2022-09-01 14:23:13 +0530

    added heading and footer

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ function App() {
 
   return (
     <div className="app">
-      {meme &&
-        meme.map((m) =>
-          m.isVideo ? (
-            <video key={m.id} src={m.url} autoPlay loop muted />
-          ) : (
-            <img key={m.id} src={m.url} alt="meme" />
-          )
-        )}
+      <h1 className="heading">Top 10 Memes From Reddit</h1>
+      <div className="meme-container">
+        {meme &&
+          meme.map((m) =>
+            m.isVideo ? (
+              <video key={m.id} src={m.url} autoPlay loop muted />
+            ) : (
+              <img key={m.id} src={m.url} alt="meme" />
+            )
+          )}
+      </div>
+      <div className="footer">
+        <span>
+          Made with ❤️ By &nbsp;
+          <a href="https://github.com/SaptarsiRoy/" className="github-url">
+            Saptarsi Roy
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
